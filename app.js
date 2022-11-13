af8f1a4239e9825838b7b4eebab40f13b82956c8
jeremycalderon <t1023600121@uitru.edu.pe>
2022-11-13 15:31:10 -0500

    patch listo

--- app.js
+++ app.js
@@ -39,6 +39,27 @@ app.post("/clientes", async (req, res) => {
     
 })
 
+app.patch('/clientes/:id', async (req, res) => {
+    const id = req.params.id
+    const { dni, apellidos, nombres, edad } = req.body
+    const cliente = {
+      dni,
+      apellidos,
+      nombres,
+      edad
+    }
+    try {
+      const updatePerson = await Cliente.updateOne({_id: id}, cliente)
+      if(updatePerson.matchedCount === 0){
+        res.status(422).json({ message: 'Usuario no encontrado'})
+        return
+      }
+      res.status(200).json(cliente)
+    } catch (error) {
+      res.status(500).json({ error: error})
+    }
+})
+
 mongoose.connect(
     `mongodb+srv://${DB_USER}:${DB_PASSWORD}@laboratorio2.gadvzad.mongodb.net/?retryWrites=true&w=majority`
     ).then(() => {
