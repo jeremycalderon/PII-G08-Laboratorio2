4724a81b828bb7f35528d61cfb4be6e4d9d4c631
CeceCF7 <106205006+CeceCF7@users.noreply.github.com>
2022-11-17 04:59:05 -0500

    Agragando los READ

--- app.js
+++ app.js
@@ -9,15 +9,11 @@ const DB_PASSWORD = "grupo08"
 
 app.use(express.json())
 
-app.get("/clientes", async (req, res) => {
-    try {
-        const clientes = await Cliente.find()
-        res.status(200).json(clientes)
-    } catch (error) {
-        res.status(500).json({error: error})
-    }
+app.get('/', (req, res) => {
+    res.json ({ message: 'hello express'})
 })
 
+//CREATE
 app.post("/clientes", async (req, res) => {
     const { dni, apellidos, nombres, edad } = req.body
     if (!dni || !apellidos || !nombres || !edad) {
@@ -39,6 +35,32 @@ app.post("/clientes", async (req, res) => {
     
 })
 
+//TOTAL READ
+app.get("/clientes", async (req, res) => {
+    try {
+        const clientes = await Cliente.find()
+        res.status(200).json(clientes)
+    } catch (error) {
+        res.status(500).json({error: error})
+    }
+})
+
+//READ CLIENTE
+app.get('/Clientes/:id', async (req, res) => {
+    const id = req.params.id
+    try {
+        const cliente = await Cliente.findOne({_id: id})
+        if(!cliente){
+            res.status(422).json({ message: 'Usuario no encontrado'})
+            return
+        }
+        res.status(200).json(cliente)
+    } catch (error) {
+        res.status(500).json({error: error})
+    }
+})
+
+//UPDATE
 app.patch('/clientes/:id', async (req, res) => {
     const id = req.params.id
     const { dni, apellidos, nombres, edad } = req.body
@@ -61,7 +83,7 @@ app.patch('/clientes/:id', async (req, res) => {
 })
 
 mongoose.connect(
-    `mongodb+srv://${DB_USER}:${DB_PASSWORD}@laboratorio2.gadvzad.mongodb.net/?retryWrites=true&w=majority`
+    `mongodb://${DB_USER}:${DB_PASSWORD}@ac-xega4kg-shard-00-00.gadvzad.mongodb.net:27017,ac-xega4kg-shard-00-01.gadvzad.mongodb.net:27017,ac-xega4kg-shard-00-02.gadvzad.mongodb.net:27017/?ssl=true&replicaSet=atlas-x26kv1-shard-0&authSource=admin&retryWrites=true&w=majority`
     ).then(() => {
         console.log("Conectado al MONGODB")
         app.listen(5000, () => {
